6aae43353ab6a9c960e8fa4a98dda3139964a7ce
jose.palacio <jose.palacio@esap.edu.co>
2025-04-10 12:40:04 +0000

    add conifg data

--- config.php
+++ config.php
@@ -4,23 +4,28 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'mariadb';
+$CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = '127.0.0.1';
-$CFG->dbname    = 'aprendetic_prod';
-$CFG->dbuser    = 'moodleuser';
-$CFG->dbpass    = 'moodlepass';
+$CFG->dbhost    = '10.10.2.7';
+$CFG->dbname    = 'aprendetic-prod';
+$CFG->dbuser    = 'aprendetic';
+$CFG->dbpass    = 'pg9$tbR8Yy!n';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => 3306,
+  'dbport' => '',
   'dbsocket' => '',
-  'dbcollation' => 'utf8mb4_unicode_ci',
+  'dbcollation' => 'utf8mb4_general_ci',
 );
 
-$CFG->wwwroot   = 'http://localhost';
-$CFG->dataroot  = '/home/azureuser/Documents/aprendetic-prod/moodledata';
+$CFG->wwwroot   = 'https://ibagueaprendetic.ibague.gov.co';
+$CFG->dataroot  = '/var/www/aprendetic/moodledata';
 $CFG->admin     = 'admin';
+$CFG->upgradekey = 'Aprendetic2024.';
+
+$CFG->debug = E_ALL | E_STRICT;        // Debug level 1: Show ALL errors
+$CFG->debugdisplay = true;            // Display errors
+//$CFG->debuglog = '/path/to/your/moodle/datafolder/debug.log';  // Log errors to a file
 
 $CFG->directorypermissions = 0777;
 
